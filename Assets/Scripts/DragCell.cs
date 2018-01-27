fefcc5da3849b4d9c5b909167c8271d19dcd7af4
Meowming <demingz1@uci.edu>
2018-01-27 13:04:24 -0800

    Drag and Drop

---
+++ Assets/Scripts/DragCell.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DragCell : MonoBehaviour {
+	public static DragCell mouseOnCell;
+	public Vector3 anchorOffset;
+	bool isEntered = false;
+	Image img;
+	public Color onEnterColor;
+	void OnMouseEnter()
+	{
+		mouseOnCell = this;
+		img.color = onEnterColor;
+		/*
+		if (DragItem.isDragging && (!isEntered))
+		{
+			Debug.Log("ss");
+			DragItem.draggingItem.transform.position = transform.position + anchorOffset;
+			isEntered = true;
+		}
+		*/
+	}
+	void OnMouseExit()
+	{
+		mouseOnCell = null;
+		img.color = Color.white;
+		//isEntered = false;
+		//renderer.material.color = originalColor;
+	}
+	// Use this for initialization
+	void Start () {
+		img = this.gameObject.GetComponent<Image>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void AlignToAnchor(DragItem item)
+	{
+		item.transform.position = transform.position + anchorOffset;
+	}
+
+}
---
+++ Assets/Scripts/DragItem.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragItem : MonoBehaviour {
+
+	static public bool isDragging = false;
+	static public DragItem draggingItem;
+	private Color mouseOverColor = Color.blue;
+	private Color originalColor = Color.yellow;
+	private bool dragging = false;
+	private float distance;
+
+	/*
+	void OnMouseEnter()
+	{
+		renderer.material.color = mouseOverColor;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = originalColor;
+	}
+	*/
+	void OnMouseDown()
+	{
+		distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		draggingItem = this;
+		isDragging = true;//global
+		dragging = true;
+	}
+
+	void OnMouseUp()
+	{
+		draggingItem = null;
+		isDragging = false;//global
+		dragging = false;
+		if (DragCell.mouseOnCell)
+		{
+			DragCell.mouseOnCell.AlignToAnchor(this);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dragging)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Vector3 rayPoint = ray.GetPoint(distance);
+			transform.position = rayPoint;
+		}
+	}
+}
