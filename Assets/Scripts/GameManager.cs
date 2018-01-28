c9587d9128f0075abd86584bbc571fd9089f3fcb
jennyzeng <jennyzengzzh@gmail.com>
2018-01-27 21:48:39 -0800

    update on scenes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : SingletonBase<GameManager> {
+	[HideInInspector]
 	public AudioClip selectedAudio;
     protected override void Init()
     {
-		DontDestroyOnLoad(gameObject);
+
     }
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
 }
