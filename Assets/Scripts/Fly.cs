84450bae9245eb95d1a4f64b4e3a2a58b9372d3e
jennyzeng <jennyzengzzh@gmail.com>
2018-01-27 22:02:36 -0800

    add ar scripts

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour {
+
+    float horizontalSpeed;
+    float verticalSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //horizontalSpeed = Random.Range(-100, 100);
+        //verticalSpeed = Random.Range(-75, 75);
+
+        horizontalSpeed = 20; // Random.Range(-100, 100);
+        //verticalSpeed = Random.Range(-75, 75);
+
+        transform.RotateAround(Camera.main.transform.position, Vector3.up, horizontalSpeed * Time.deltaTime);
+        //transform.RotateAround(Camera.main.transform.position, Vector3.forward, verticalSpeed * Time.deltaTime);
+
+    }
+}
