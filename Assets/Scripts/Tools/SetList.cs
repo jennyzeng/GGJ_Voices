daa1332aa912b4ce13a129e552cf8d4d610ac714
Meowming <demingz1@uci.edu>
2018-01-28 01:07:02 -0800

    Manually modify Set list

---
+++ Assets/Scripts/Tools/SetList.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SetList : MonoBehaviour {
+
+	public List<DragCell> cells = new List<DragCell>();
+	public List<DragItem> items = new List<DragItem>();
+	// Use this for initialization
+	void Start () {
+		DragCell.cellList = cells;
+		DragItem.itemList = items;
+	}
+}
